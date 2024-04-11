890e1ffb9c83fe3016f7de904b7d1938c0eebfd0
Lawrence-AIP <liuch@allinfinance.com>
2024-04-11 16:35:51 +0800

    docs: 完善02,但是Bert理解还不够深入

--- 02-Bert 文本分类/utils.py
+++ 02-Bert 文本分类/utils.py
@@ -38,21 +38,63 @@ class MyDataset(Dataset):
         self.tokenizer = BertTokenizer.from_pretrained(parsers().bert_pred)
 
     def __getitem__(self, index):
-        # 取出一条数据并截断长度
+        # 获取数据集中索引为 index 的文本数据，并截断至最大长度 max_len。
         text = self.all_text[index][:self.max_len]
         label = self.all_label[index]
 
         # 分词
         text_id = self.tokenizer.tokenize(text)
-        # 加上起始标志
+        # 在分词结果的开头加上 [CLS] 标记，表示序列的开头，这是 BERT 模型中的特殊标记之一。
         text_id = ["[CLS]"] + text_id
 
-        # 编码
+        # 编码  将分词后的文本转换为对应的词汇表中的 ID。
         token_id = self.tokenizer.convert_tokens_to_ids(text_id)
-        # 掩码  -》
+
+        """
+        mask = [1] * len(token_id)：首先，将整个文本的长度设为1（真实标记），因为现在只考虑文本中的真实标记，
+        即除了填充标记和特殊标记以外的部分。
+        + [0] * (self.max_len + 2 - len(token_id))：然后，将填充部分的长度（即最大长度减去真实标记的长度）设为0（填充标记），
+        这样做是因为填充部分不是真实的标记，应该被屏蔽掉。
+        
+        我们假设有以下数据：
+
+        文本： "Hello, how are you?"
+        最大长度： 10
+        首先，我们使用BERT分词器对文本进行分词，得到标记列表：
+        token_ids=[101,7592,1010,2129,2024,2017,1029,102]
+        
+        其中：
+        
+        101 是 [CLS] 标记的ID
+        102 是 [SEP] 标记的ID
+        接下来，我们根据最大长度和标记列表的长度来生成掩码列表：
+        真实标记部分的掩码：
+            mask(real)=[1,1,1,1,1,1,1,1]
+        填充标记部分的掩码:
+            mask(pad)=[0,0,0]
+        将这两部分拼接在一起，得到最终的掩码列表：   
+        mask = [1,1,1,1,1,1,1,1,0,0,0]
+        这样，我们就得到了针对该文本的掩码列表，用于标识哪些位置是真实的标记，哪些位置是填充的标记。 
+        
+        
+        掩码（Mask）在自然语言处理中扮演着重要的角色，特别是在序列模型中。在BERT模型中，掩码的作用主要有两个方面：
+
+        填充屏蔽（Padding Masking）：在输入序列中，通常会对长度不一致的句子进行填充操作，使它们的长度相同。
+        但是在计算中，我们不希望填充部分的内容对结果产生影响。因此，通过掩码将填充部分的影响屏蔽掉，使模型不会考虑这些填充标记。
+        
+        语言模型的预训练：在BERT的预训练阶段，掩码还可以用于掩盖部分输入，让模型学会对缺失部分进行预测。
+        这种机制被称为掩码语言模型（Masked Language Model, MLM）。在训练过程中，一些输入的标记将被随机掩码（例如用 [MASK] 标记替换），
+        而模型需要预测这些掩码的位置对应的原始标记。这样的训练方式可以使模型更好地理解上下文和语言的语义。
+        """
+        # 掩码  创建一个掩码列表，用于标识哪些位置是真实的标记，哪些位置是填充的标记。这里的 [CLS] 和填充标记 [PAD] 都被视为真实标记。
         mask = [1] * len(token_id) + [0] * (self.max_len + 2 - len(token_id))
-        # 编码后  -》长度一致
+        # 编码后  将编码后的标记列表填充到最大长度 max_len，并加上 [CLS] 和 [SEP]（在这里没有显示，但通常在 BERT 模型中，在句子的最后会加上一个 [SEP] 标记）。
         token_ids = token_id + [0] * (self.max_len + 2 - len(token_id))
+
+        # 填充到最大长度并加上特殊标记 [CLS] 和 [SEP] (上一句这这儿2句只能选一个)
+        token_ids += [self.tokenizer.pad_token_id] * (self.max_len - len(token_ids))
+        token_ids = [self.tokenizer.cls_token_id] + token_ids + [self.tokenizer.sep_token_id]
+
         # str -》 int
         label = int(label)
 
